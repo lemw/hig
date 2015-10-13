46258c80fa0e76ec9d4a84fce52660afec887adb
Lars Wattsgard <lars@wattsgard.se>
2015-10-13 12:25:06 +0200

    First commit

---
+++ HelloWorld/gen/hig/androidkurs/examples/helloandorid/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package hig.androidkurs.examples.helloandorid;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
